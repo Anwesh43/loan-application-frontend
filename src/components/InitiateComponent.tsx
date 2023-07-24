4f43dfafc3e2d14661b375e1f996a2d11bedbfe7
Anwesh43 <anweshthecool0@gmail.com>
2023-07-25 00:46:38 +0530

    adding chakra setup

---
+++ src/components/InitiateComponent.tsx
@@ -0,0 +1,8 @@
+import { VStack } from "@chakra-ui/react"
+
+const InitiateComponent = () => {
+    
+    return <VStack h = "max-content" w = "max-content" spacing={10}
+        
+    ></VStack>
+}
